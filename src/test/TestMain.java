5c0a05cea987f384ae021dc0874293a0af118972
fonoisrev <wuhangdeyouxiang@gmail.com>
2012-09-23 16:32:31 +0800

    创建项目，导入测试用的文件

---
+++ src/test/TestMain.java
@@ -0,0 +1,9 @@
+package test;
+
+import wxy.Main;
+
+public class TestMain {
+	public static void main(String[] args){
+		new Main().print("Hello world!");
+	}
+}
---
+++ src/wxy/Main.java
@@ -0,0 +1,7 @@
+package wxy;
+
+public class Main {
+	public void print(String string){
+		System.out.println(string);
+	}
+}
